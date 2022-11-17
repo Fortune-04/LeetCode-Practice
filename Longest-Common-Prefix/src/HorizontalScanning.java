17e6fc4df30e9ea95773958aed9808ee90f26b3a
Fortune-04 <62495862+Fortune-04@users.noreply.github.com>
2022-11-18 03:49:15 +0800

    Added leet code

---
+++ Longest-Common-Prefix/src/HorizontalScanning.java
@@ -0,0 +1,10 @@
+public class HorizontalScanning {
+    public static void main(String[] args) {
+        String word = "test";
+        String word2 = "te";
+
+//        System.out.println(word.indexOf(word2));
+        System.out.println(word.substring(1));
+
+    }
+}
